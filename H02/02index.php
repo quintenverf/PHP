1d090303ed61f3fe5996dadb09fe6af97794c628
quinten <qbverf@gmail.com>
2021-01-18 14:23:28 +0100

    h02 finished and finished making sentences.

--- H02/02index.php
+++ H02/02index.php
@@ -1,4 +1,26 @@
 <?php
 
 $aantal = 14;
-echo $aantal;
+
+$deze_jongen = "deze jongen is";
+
+echo $deze_jongen.$aantal." jaar oud";
+
+$string = "//////////";
+echo $string;
+
+$a = "ik heb niet gefaald";
+$b = "die niet werken";
+$c = ". Ik heb alleen";
+$d = " manieren gevonden ";
+$e = 1000;
+
+echo $a.$c.$e.$d.$b;
+
+$q = "mensen die geen ";
+$w = "fouten maken";
+$r = ", werken niet.";
+
+echo $string;
+
+echo $q.$w.$r;
