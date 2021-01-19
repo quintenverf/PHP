fcbbe775b5e2a78f1ab32f09072ece6b3fea1a48
quinten <qbverf@gmail.com>
2021-01-19 10:25:17 +0100

    boom opdr toegevoegd

--- H03/opdrAap.php
+++ H03/opdrAap.php
@@ -10,12 +10,19 @@
 </head>
 <body>
 <?php
-for ($i = 1; $i<=9; $i++) {
-    echo "<img src='img/foto". $i .".jpg>'>";
+//for ($i = 1; $i<=9; $i++) {
+////    echo "<img src='img/foto". $i .".jpg>'>";
+////}
+////
+////echo "de afbeeldingen showen niet";
+
+$bomen = array("boompie.jpg", "boompje.jpg", "downlaod.jfif", "kaBoom.jpg");
+
+foreach ($bomen as $boom) {
+    echo "<img src='img/bomen/".$boom."'>";
 }
 
-echo "de afbeeldingen showen niet";
 ?>
-<img src="img/foto1.jpg">
+<!--<img src="img/foto1.jpg">-->
 </body>
 </html>
