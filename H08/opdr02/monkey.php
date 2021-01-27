504bda549daec9c2db2cb6155ba0b25b3435ee5d
quinten <qbverf@gmail.com>
2021-01-27 14:12:06 +0100

    H08 voortgang opdr 2 toegevoegd

--- H08/opdr02/monkey.php
+++ H08/opdr02/monkey.php
@@ -1,1 +1,49 @@
 <?php
+
+$apen[] = "Baviaan";
+$apen[] = "Guereza";
+$apen[] = "Langoer";
+$apen[] = "Neusaap";
+$apen[] = "Tamarin";
+$apen[] = "Brulaap";
+$apen[] = "Halfaap";
+$apen[] = "Mandril";
+
+?>
+
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="monkey.css">
+    <link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<header>
+    <img id="logo" src="monkey-business%20(1).jpg" alt="logo_monkey_business">
+    <h3>select your monkey!</h3>
+    <img id="logo" src="monkey_swings%20(1).png" alt="text_pillaar">
+</header>
+
+<a class="aap" href="https://www.google.nl/search?q=Baviaan&tbm=isch ">Baviaan</a><br>
+
+<a class="aap" href="https://www.google.nl/search?q=guereza&tbm=isch ">guereza</a><br>
+
+<a class="aap" href="https://www.google.nl/search?q=langoer&tbm=isch ">langoer</a><br>
+
+<a class="aap" href="https://www.google.nl/search?q=neusaap&tbm=isch ">neusaap</a><br>
+
+<a class="aap" href="https://www.google.nl/search?q=tamarin&tbm=isch ">tamarin</a><br>
+
+<a class="aap" href="https://www.google.nl/search?q=brulaap&tbm=isch ">brulaap</a><br>
+
+<a class="aap" href="https://www.google.nl/search?q=halfaap&tbm=isch ">halfaap</a><br>
+
+<a class="aap" href="https://www.google.nl/search?q=mandril&tbm=isch ">mandril</a><br>
+
+</body>
+</html>
