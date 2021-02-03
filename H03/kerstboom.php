6995b3dc53c15624138f454df10e53eb8f463754
quinten <qbverf@gmail.com>
2021-02-03 09:31:15 +0100

    toets aantekeningen toegevoegd

--- H03/kerstboom.php
+++ H03/kerstboom.php
@@ -27,6 +27,54 @@ for ($x = 35;$x >= 25; $x--) {
     echo " hoppelepee ";
 }
 
+array();
+for ($i = 0; $i < amount; $i++) {
+    $array[] = rand(0 > 20, 20 > 100);
+    $strevv = rand();
+}
+
+
+$array = array(
+    'a'  => 1,
+    'ja' => 4,
+    'c'  => 3
+);
+echo 1.4;
+
+if ($weer = true) {
+    echo "breng je paraplu";
+} else {
+    echo "laat het lekker thuis!";
+}
+
+$maandag = 1;
+$dinsdag = 0;
+$woensdag = 0;
+$donderdag = 1;
+$vrijdag = 1;
+$zaterdag = 0;
+$zondag = 0;
+$array($maandag,$dinsdag, $woensdag, $donderdag, $vrijdag, $zaterdag,$zondag);
+if ($array = 1) {
+    $array =true;
+} else {
+    $array = 0;
+    $array = false;
+}
+
+
+function concatenateString($stringOne, $stringTwo, $stringThree) {
+    return $stringOne. $stringTwo. $stringThree;
+}
+
+if ($strevv = true) {
+    echo "ja t is een palindroom";
+} else {
+    echo"nee dat is t niet";
+}
+
+
+
 ?>
 </body>
 </html>
