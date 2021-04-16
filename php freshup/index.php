9d541c0c66f7a967d1ffd6e6b4185d89cdebcbe9
quinten <qbverf@gmail.com>
2021-04-16 08:50:34 +0200

    php opfrissen

--- php freshup/index.php
+++ php freshup/index.php
@@ -40,3 +40,66 @@ if( $a > $b ) {
 }else {
     echo 'a and b appers to be equal';
 }
+    // ternary operations
+// long version
+$is_logged_in = true;
+if ($is_logged_in) {
+    $message = "welcome back!";
+} else {
+    $message = "intruder!!";
+}
+
+// short version
+$message = $is_logged_in ? "welcome back!" : "intruder!!";
+
+// even shorter version for writing one thing
+echo "welcome " . ($is_logged_in ? " back!" : "!");
+
+echo '<pre>';
+echo $message;
+echo '<pre>';
+
+
+ // so what this basically tells you is the following:
+// name gets, if name is already set then display name and if not than display 'joe';
+$name = isset( $name ) ? $name : 'joe';
+
+// the shorter version is the elvis operator
+// and tells you 'if name is null then display joe'
+$name = null;
+$name = $name ?: 'joe';
+echo $name;
+
+//SWITCH STATEMENTS
+//switches with cases and echo's
+$total = 1;
+switch ($total) {
+    case 1:
+        echo '<p>$total is 1</p>';
+        break;
+    case 2:
+        echo '<p>$total is 2</p>';
+        break;
+    case 3:
+        echo '<p>$total is 3</p>';
+        break;
+    default:
+        echo '<p>$total is more than 3</p>';
+}
+
+//simplifying the switch statement by putting it together
+switch ($total){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        echo '<p>$total is less than 5</p>';
+        break;
+    default:
+        echo '<p>$total is above 5</p>';
+}
+
+//challenge
+
+
+
